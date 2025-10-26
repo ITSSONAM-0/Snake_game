1ea29b954a96bffbc9d4d32f3fe1368d8e7c9c1c
sonam kumari <135491610+ITSSONAM-0@users.noreply.github.com>
2025-10-26 20:18:50 +0530

    Update snake_game.py

--- snake_game.py
+++ snake_game.py
@@ -2,17 +2,17 @@ import pygame
 import time
 import random
 
-# Initialize pygame
+
 pygame.init()
 
-# Colors
+
 white = (255, 255, 255)
 black = (0, 0, 0)
 red = (213, 50, 80)
 green = (0, 255, 0)
 blue = (50, 153, 213)
 
-# Screen size
+
 dis_width = 600
 dis_height = 400
 dis = pygame.display.set_mode((dis_width, dis_height))
